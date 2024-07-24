4ba328725293083d9a33a7b42bd9b6c4e21da707
asp <asp@aptech.com>
2024-07-24 12:34:40 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { OteComponent } from './ote/ote.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,OteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig : {
+    apiKey: "AIzaSyA5nGFU4CICeV3Vyn4kHbKdgK0JEYZhSx4",
+    authDomain: "notes-89a0e.firebaseapp.com",
+    projectId: "notes-89a0e",
+    storageBucket: "notes-89a0e.appspot.com",
+    messagingSenderId: "648152106081",
+    appId: "1:648152106081:web:54029ad8877e55df41d620"
+  },
+production: false
+};
